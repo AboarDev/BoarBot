3318361311d1b7293f2986e427b1c18aa5e592ac
Sam <sksmiller@gmail.com>
2020-12-20 16:30:16 +1300

    Cleaned up code

--- scrape.py
+++ scrape.py
@@ -15,33 +15,16 @@ class Scrape():
     async def getPercentage (self,msg,txt):
         async with msg.channel.typing():
             fullTotal = -1
-            counter = 0
             theTime = datetime.datetime.today()
-            members = {
-
-            }
-            messageCount = {
-
-            }
-            theBefore = None
-            iterate = True
-            while iterate:
-                async for message in msg.channel.history(before=theBefore, limit=100):
-                    if not message.author.id in members:
-                        members[message.author.id] = message.author.display_name
-                        messageCount[message.author.id] = 1
-                    else:
-                        messageCount[message.author.id] += 1
-                    fullTotal += 1
-                    counter += 1
-                    if counter == 100:
-                        theBefore = message
-                print(counter)
-                if counter == 100:
-                    iterate = True
-                    counter = 0
+            members = {}
+            messageCount = {}
+            async for message in msg.channel.history(limit=None):
+                if not message.author.id in members:
+                    members[message.author.id] = message.author.display_name
+                    messageCount[message.author.id] = 1
                 else:
-                    iterate = False
+                    messageCount[message.author.id] += 1
+                fullTotal += 1
             output = f'```Total Messages in {msg.channel.name} - {fullTotal}\n'
             def by_value(item):
                 return item[1]
@@ -63,26 +46,15 @@ class Scrape():
         }
         theBefore = None
         await msg.delete()
-        iterate = True
-        while iterate:
-            async for message in msg.channel.history(before=theBefore, limit=100):
-                if not message.author.id in output['members']:
-                    output['members'][message.author.id] = message.author.display_name
-                theAttachments = []
-                for attachment in message.attachments:
-                    theAttachments.append(attachment.url)
-                output['messages'].append({'id': str(message.id), 'author': str(
-                    message.author.id), 'attachments': theAttachments, 'createdAt': message.created_at.__str__(), 'txt': message.content})
-                fullTotal += 1
-                counter += 1
-                if counter == 100:
-                    theBefore = message
-            print(counter)
-            if counter == 100:
-                iterate = True
-                counter = 0
-            else:
-                iterate = False
+        async for message in msg.channel.history(limit=None):
+            if not message.author.id in output['members']:
+                output['members'][message.author.id] = message.author.display_name
+            theAttachments = []
+            for attachment in message.attachments:
+                theAttachments.append(attachment.url)
+            output['messages'].append({'id': str(message.id), 'author': str(
+                message.author.id), 'attachments': theAttachments, 'createdAt': message.created_at.__str__(), 'txt': message.content})
+            fullTotal += 1
         output['messages'].reverse()
         print(fullTotal)
         if txt.find("-send") >= 0:
